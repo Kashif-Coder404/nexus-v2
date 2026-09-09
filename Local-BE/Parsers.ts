e35c5c91ae842556ffc997d85bc627a0878b8c30
Kashif-Coder404 <kashifahmead8755@gmail.com>
2026-09-09 22:15:19 +0530

    refactor(local-be): remove application launch interception feature

--- Local-BE/Parsers.ts
+++ Local-BE/Parsers.ts
@@ -1,9 +1,5 @@
 import { executeCmd, ExecutionResponse } from "./services/execute.service";
 import {
-  isAppLaunchCommand,
-  launchAppWithVerification,
-} from "./services/launchApp.service";
-import {
   nexusSmartSearch,
   nexusSmartSearchApp,
 } from "./services/search.service";
@@ -105,18 +101,6 @@ export const commandParser = async (
           : undefined;
     },
     in_built: async () => {
-      const rawParam = typeof cmd.param === "string" ? cmd.param : "";
-      if (isAppLaunchCommand(rawParam)) {
-        const launchResult = await launchAppWithVerification(rawParam);
-        finalResponse.cmd = returningCmd;
-        finalResponse.msg = "";
-        finalResponse.terminalOutput = launchResult.stdout;
-        finalResponse.terminalError = launchResult.stderr;
-        finalResponse.exitCode = launchResult.exitCode;
-        finalResponse.isSuccess = launchResult.isSuccess;
-        return;
-      }
-
       const timeoutMs =
         cmd.timeout && !isNaN(Number(cmd.timeout))
           ? Number(cmd.timeout)
